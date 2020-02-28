1082d43f8c181d9648aa30d788a392ef01aa4aba
angelopoole <angelo.poole@outlook.com>
2020-02-28 18:03:09 -0500

    login page visualy functioning

--- src/ComponentContainers/loginPage.jsx
+++ src/ComponentContainers/loginPage.jsx
@@ -4,18 +4,20 @@ import anime from 'animejs'// import React, { Component } from 'react'
 
 // let kom = null;
 export default class Login extends React.Component {
+    
+    constructor(props) {
+        super(props)
+        this.current = null
+    
+        this.state = {
+        }
+    }
+    
 
     userNameFocusHandler = (e) => {
-        let current = null;
-    }
-
-
-    componentDidMount() {
-
-        let current = null;
-        document.querySelector('#username').addEventListener('focus', function (e) {
-            if (current) current.pause();
-            current = anime({
+        // let current = null;
+        if (this.current) this.current.pause();
+            this.current = anime({
                 targets: 'path',
                 strokeDashoffset: {
                     value: 0,
@@ -28,10 +30,11 @@ export default class Login extends React.Component {
                     easing: 'easeOutQuart'
                 }
             });
-        });
-        document.querySelector('#password').addEventListener('focus', function (e) {
-            if (current) current.pause();
-            current = anime({
+    }
+
+    passwordFocusHandler = (e) => {
+        if (this.current) this.current.pause();
+            this.current = anime({
                 targets: 'path',
                 strokeDashoffset: {
                     value: -336,
@@ -44,10 +47,12 @@ export default class Login extends React.Component {
                     easing: 'easeOutQuart'
                 }
             });
-        });
-        document.querySelector('#submit').addEventListener('focus', function (e) {
-            if (current) current.pause();
-            current = anime({
+    }
+
+    submitFocusHandler = (e) => {
+        // e.preventDefault();
+        if (this.current) this.current.pause();
+            this.current = anime({
                 targets: 'path',
                 strokeDashoffset: {
                     value: -730,
@@ -60,7 +65,14 @@ export default class Login extends React.Component {
                     easing: 'easeOutQuart'
                 }
             });
-        });
+    }
+    
+    SubmitHandler = (e) => {
+        e.preventDefault()
+    }
+
+    componentDidMount() {
+
     }
 
     render() {
@@ -76,7 +88,7 @@ export default class Login extends React.Component {
                         <svg viewBox="0 0 320 300">
                             <defs>
                                 <linearGradient
-                                    inkscapeCollect="always"
+                                    inkscapecollect="always"
                                     id="linearGradient"
                                     x1="13"
                                     y1="193.49992"
@@ -95,12 +107,12 @@ export default class Login extends React.Component {
                             </defs>
                             <path d="m 40,120.00016 239.99984,-3.2e-4 c 0,0 24.99263,0.79932 25.00016,35.00016 0.008,34.20084 -25.00016,35 -25.00016,35 h -239.99984 c 0,-0.0205 -25,4.01348 -25,38.5 0,34.48652 25,38.5 25,38.5 h 215 c 0,0 20,-0.99604 20,-25 0,-24.00396 -20,-25 -20,-25 h -190 c 0,0 -20,1.71033 -20,25 0,24.00396 20,25 20,25 h 168.57143" />
                         </svg>
-                        <form className="form" autocomplete="off">
+                        <form className="form" onSubmit={ e => this.SubmitHandler(e)} >
                             <label for="username">username</label>
-                            <input type="username" autoComplete='off' id="username" />
-                            <label for="password">Password</label>
-                            <input type="password" id="password" />
-                            <input type="submit" id="submit" value="Submit" />
+                            <input type="username" autoComplete='off' onFocus={(e) => this.userNameFocusHandler(e)} id="username" />
+                            <label for="password" >Password</label>
+                            <input type="password" onFocus={(e) => this.passwordFocusHandler(e)} id="password" />
+                            <input type="submit" onFocus={(e) => this.submitFocusHandler(e)} id="submit" value="Submit" />
                         </form>
                     </div >
 
